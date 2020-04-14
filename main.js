24c3e923682535bbc9ebc6f1722ca12c0ab75ef7
Luca Agosta <me@agosta.dev>
2020-04-14 18:36:25 -0300

    Update

---
+++ main.js
@@ -0,0 +1,29 @@
+function expFunc() {
+    var d = new Date();
+    var y = d.getFullYear();
+    x = y - 2001
+    document.getElementById("experiencia").innerHTML = "Mais de " + x + " anos de experiência.";
+} 
+
+
+/*
+Returns a list of reviews.
+@param locationName Name of the location to retrieve reviews for.
+@return List<Reviews> A list of reviews.
+@throws Exception
+
+locationName = "Advocadia Marcos Monteiro Cândido"
+adv MMC = Place ID: ChIJL1 - 59c4Dz5QRVwC5xDoahqA
+
+public static List < Review > listReviews(String locationName) throws Exception {
+    Mybusiness.Accounts.Locations.Reviews.List reviewsList =
+        mybusiness.accounts().locations().reviews().list(locationName);
+    ListReviewsResponse response = accountsList.execute();
+    List < Reviews > reviews = response.getReviews();
+
+    for (Reviews review : reviews) {
+        System.out.println(review.toPrettyString());
+    }
+    return reviews;
+}
+*/
